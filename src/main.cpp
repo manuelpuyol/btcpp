0d2f151990b5681cfc4985c2557e3bac211e09a7
Manuel Puyol <manuelpuyol@gmail.com>
2020-06-04 14:33:11 -0500

    uncomment not cuda code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-// #include<cli.hpp>
-// #include<json.hpp>
+#include<cli.hpp>
+#include<json.hpp>
 
 #ifdef USE_CUDA
 #include<cuda_sha256.hpp>
@@ -12,8 +12,8 @@ int main() {
   cuda_sha256("8e3b03f9c2e60bb03a225f3be4d63a4a08445d34e76712e0667fad8d6000f4722305843009213694060", 3);
   cuda_sha256("8e3b03f9c2e60bb03a225f3be4d63a4a08445d34e76712e0667fad8d6000f4722305843009213694060", 4);
 #else
-  // CLI cli;
+  CLI cli;
 
-  // cli.run();
+  cli.run();
 #endif
 }
